620b7f3b3486a9aedf15f21712c1dc50fd7b16b0
Manav Gupta <99027134+MM21B038@users.noreply.github.com>
2024-08-08 03:00:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_model_package',
+    name='its_a_trial',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
